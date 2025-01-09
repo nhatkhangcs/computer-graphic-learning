420c00201300f1242e046eddae215b159421989c
Khang Vo <nhatkhangdtp@gmail.com>
2025-01-09 16:46:43 +0700

    2.4 2.5

--- src/main/test-2-1.py
+++ src/main/test-2-1.py
@@ -3,10 +3,12 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from core.base import Base
+from core.openGLUtils import OpenGLUtils
 
 
 class Test(Base):
     def initialize(self):
+        OpenGLUtils.printSystemInfo()
         print("initialize")
 
     def update(self):
--- src/main/test-2-5.py
+++ src/main/test-2-5.py
@@ -3,40 +3,44 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 from core.base import Base
 from core.openGLUtils import OpenGLUtils
 from core.attribute import Attribute
 from OpenGL.GL import *
 
+# render shapes with vertex colors
 
-# render six points in a hexagon arrangement
+
 class Test(Base):
     def initialize(self):
         print("Initializing program...")
         ### initialize program ###
         vsCode = """
-            in vec3 position;
-            void main()
-            {
+        in vec3 position;
+        in vec3 vertexColor;
+        out vec3 color;
+        void main()
+        {
             gl_Position = vec4(position.x, position.y, position.z, 1.0);
-            }
+            color = vertexColor;
+        }
         """
         fsCode = """
-            out vec4 fragColor;
-            void main()
-            {
-            fragColor = vec4(1.0, 1.0, 0.0, 1.0);
-            }
+        in vec3 color;
+        out vec4 fragColor;
+        void main()
+        { 
+            fragColor = vec4(color.r, color.g, color.b, 1.0);
+        }
         """
         self.programRef = OpenGLUtils.initializeProgram(vsCode, fsCode)
-
         ### render settings (optional) ###
+        glPointSize(10)
         glLineWidth(4)
         ### set up vertex array object ###
         vaoRef = glGenVertexArrays(1)
         glBindVertexArray(vaoRef)
-        ### set up vertex attribute ###
+        ### set up vertex attributes ###
         positionData = [
             [0.8, 0.0, 0.0],
             [0.4, 0.6, 0.0],
@@ -48,10 +52,20 @@ class Test(Base):
         self.vertexCount = len(positionData)
         positionAttribute = Attribute("vec3", positionData)
         positionAttribute.associateVariable(self.programRef, "position")
+        colorData = [
+            [1.0, 0.0, 0.0],
+            [1.0, 0.5, 0.0],
+            [1.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 0.0, 1.0],
+            [0.5, 0.0, 1.0],
+        ]
+        colorAttribute = Attribute("vec3", colorData)
+        colorAttribute.associateVariable(self.programRef, "vertexColor")
 
     def update(self):
         glUseProgram(self.programRef)
-        glDrawArrays(GL_TRIANGLE_STRIP_ADJACENCY, 0, self.vertexCount)
+        glDrawArrays(GL_POINTS, 0, self.vertexCount)
 
 
 if __name__ == "__main__":
